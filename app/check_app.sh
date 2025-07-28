b9cb4edf0f72ea05596ca1a96e0baece2443ac45
Cyril GENISSON <cyril.genisson@laplateforme.io>
2025-07-28 13:51:31 +0200

    [APP] add index file and sanity checks

---
+++ app/check_app.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# COLORS
+GREEN='\e[32m'
+RED='\e[31m'
+NC='\e[0m'
+
+CODE_OUTPUT=0
+
+check() {
+    echo -n "index.html file exist: "
+    if [ -f index.html ]; then
+        echo -e "${GREEN}PASS${NC}"
+    else
+        echo -e "${RED}FAULT${NC}"
+        CODE_OUTPUT=1
+    fi
+}
+
+
+echo "Start verifications:"
+echo "--------------------"
+echo ""
+check
+echo ""
+echo "-----------------"
+echo "End verifications"
+echo "-----------------"
+echo "Return: $CODE_OUTPUT"
